16d8c125689c75554e345db6393c188940ce10ac
minjunss <jomj2214@gmail.com>
2022-12-20 00:05:29 +0900

    oauth2

--- src/main/java/TDT/backend/controller/LoginController.java
+++ src/main/java/TDT/backend/controller/LoginController.java
@@ -1,19 +1,22 @@
 package TDT.backend.controller;
 
 import TDT.backend.security.dto.SessionMember;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
+@Slf4j
 public class LoginController {
 
-    @GetMapping("/login")
-    public @ResponseBody String  login(String code) {
-        ModelAndView mv = new ModelAndView();
-        System.out.println(code);
-        return code;
+    @PostMapping("/users")
+    public ResponseEntity login(@RequestBody String name) {
+        log.info("user: {}", name);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/login/google")
