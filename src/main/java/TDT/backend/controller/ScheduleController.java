ab3a55a01ed9fb67ad00fa2c164df6c85e48d106
jungsolsol <jsol78@naver.com>
2023-01-02 20:19:00 +0900

    서비스로직(스터디추가시 스터디원에게 웹푸시알림)추가

--- src/main/java/TDT/backend/controller/ScheduleController.java
+++ src/main/java/TDT/backend/controller/ScheduleController.java
@@ -3,6 +3,7 @@ package TDT.backend.controller;
 import TDT.backend.dto.schedule.ScheduleRequestDto;
 import TDT.backend.dto.schedule.ScheduleResForMember;
 import TDT.backend.dto.schedule.ScheduleResForTeam;
+import TDT.backend.service.FCMService;
 import TDT.backend.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,12 +18,12 @@ import java.util.List;
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
+    private final FCMService fcmService;
 
     @PostMapping
     public ResponseEntity<?> addSchedule(@RequestBody ScheduleRequestDto dto) {
-
         scheduleService.addSchedule(dto);
-
+        fcmService.sendNotification(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
--- src/main/java/TDT/backend/repository/teamMember/TeamMemberRepository.java
+++ src/main/java/TDT/backend/repository/teamMember/TeamMemberRepository.java
@@ -5,17 +5,23 @@ import TDT.backend.entity.Team;
 import TDT.backend.entity.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long>{
 
 
     Optional<TeamMember> findByTeamId(Long teamId);
-    Optional<Team> findByTeam(Team team);
+
+    List<TeamMember> findAllByTeamId(Long teamId);
+
+
+
 
     @Query("select tm from TeamMember tm where tm.member.id = :memberId and tm.team.id = :teamId")
-    Optional<TeamMember> findByMemberIdAndTeamId(Long memberId, Long teamId);
+    Optional<TeamMember> findByMemberIdAndTeamId(@Param("memberId") Long memberId, @Param("teamId") Long teamId);
 
 
 }
