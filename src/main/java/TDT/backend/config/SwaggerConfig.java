f3a241df3e885aee1b8a79186c6ddbcaf180be62
jungsolsol <jsol78@naver.com>
2023-01-03 18:41:54 +0900

    +Swagger

---
+++ src/main/java/TDT/backend/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package TDT.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                // 스웨거가 RestController를 전부 스캔을 한다.
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("스터디 일정관리 API 서비스")
+                .description("스터디 일정관리 API 서비스 입니다")
+                .version("0.1.0")
+                .license("LICENSE")
+                .licenseUrl("")
+                .build();
+    }
+
+    // 완료가 되었으면 오른쪽 URL 로 접속 => http://localhost:8080/swagger-ui.html
+
+}
--- src/main/java/TDT/backend/controller/LoginController.java
+++ src/main/java/TDT/backend/controller/LoginController.java
@@ -5,12 +5,12 @@ import TDT.backend.dto.member.ProfileReqDto;
 import TDT.backend.dto.member.ProfileResDto;
 import TDT.backend.entity.Member;
 import TDT.backend.service.MemberService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
 
 
 @RestController
@@ -20,6 +20,7 @@ import java.io.IOException;
 public class LoginController {
 
     private final MemberService memberService;
+    @ApiOperation(value = "로그인",notes = "회원 로그인")
     @PostMapping
     public ResponseEntity<?> login(@RequestBody InsertMemberReq req) {
         log.info(req.getName());
@@ -28,12 +29,14 @@ public class LoginController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "회원 탈퇴",notes = "회원 탈퇴")
     @DeleteMapping
     public ResponseEntity<?> signOutMember(@RequestParam("memberId") Long memberId) {
         memberService.deleteMember(memberId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "프로필 작성",notes = "회원 프로필 작성")
     @PatchMapping("/edit")
     public ResponseEntity<?> addProfile(@RequestBody ProfileReqDto dto) {
         memberService.addProfile(dto);
