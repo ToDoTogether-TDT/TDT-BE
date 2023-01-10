22179ddfa422fd90b1e6b13ac0b3c6dad1ffe03a
minjunss <jomj2214@gmail.com>
2023-01-10 22:38:08 +0900

    SecurityConfig 수정

--- src/main/java/TDT/backend/config/AppConfig.java
+++ src/main/java/TDT/backend/config/AppConfig.java
@@ -4,11 +4,13 @@ import TDT.backend.common.auth.jwt.JwtAuthenticationFilter;
 import TDT.backend.common.auth.jwt.JwtTokenProvider;
 import TDT.backend.common.auth.jwt.MemberAuthenticationEntryPoint;
 import TDT.backend.service.LoginService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,15 +18,11 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AppConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final LoginService loginService;
-
-    public AppConfig(JwtTokenProvider jwtTokenProvider, LoginService loginService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.loginService = loginService;
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -35,6 +33,9 @@ public class AppConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable().cors().disable()
+                .formLogin().disable().httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
                 .and()
